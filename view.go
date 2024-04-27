616c4020bbab8753ec0f5cb2bf9829fcc06fb07c
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-04-27 22:31:09 +0100

    Remove unused code

--- view.go
+++ view.go
@@ -8,23 +8,3 @@ type view interface {
 	update(msg tea.KeyMsg, m model) (tea.Model, tea.Cmd)
 	draw(m model) string
 }
-
-//type keyMap struct {
-//	Up    key.Binding
-//	Down  key.Binding
-//	Left  key.Binding
-//	Right key.Binding
-//	Help  key.Binding
-//	Quit  key.Binding
-//}
-//
-//func (k keyMap) ShortHelp() []key.Binding {
-//	return []key.Binding{k.Help, k.Quit}
-//}
-//
-//func (k keyMap) FullHelp() [][]key.Binding {
-//	return [][]key.Binding{
-//		{k.Up, k.Down, k.Left, k.Right},
-//		{k.Help, k.Quit},
-//	}
-//}q
